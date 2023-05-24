e4c778ca4f0b1f30944610bdaf8c1de658f03948
jose medina tortosa <josemedinatortosa@gmail.com>
2023-05-24 21:24:47 +0200

    Creadas las clases de los modelos

---
+++ Negocio/Clases/LogicaUsuario.cs
@@ -0,0 +1,27 @@
+﻿using ProyectoExtreme_Datos;
+using ProyectoExtreme_Negocio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoExtreme_Negocio.Clases
+{
+    public class LogicaUsuario : IUsuario
+    {
+        private ProyectoExtremeContext db;
+
+        public LogicaUsuario(ProyectoExtremeContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Usuario> GetUsuarios()
+        {
+            return db.Usuarios.ToList();
+        }
+            
+
+    }
+}
---
+++ Negocio/Clases/LogicaVehiculo.cs
@@ -0,0 +1,25 @@
+﻿using ProyectoExtreme_Datos;
+using ProyectoExtreme_Negocio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoExtreme_Negocio.Clases
+{
+    public class LogicaVehiculo : IVehiculo
+    {
+        private ProyectoExtremeContext db;
+
+        public LogicaVehiculo(ProyectoExtremeContext db)
+        {
+            this.db = db;
+        }
+
+        public List<Vehiculo> GetVehiculos()
+        {
+            return db.Vehiculos.ToList();
+        }
+    }
+}
