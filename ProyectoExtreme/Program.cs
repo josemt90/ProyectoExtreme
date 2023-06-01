7dcf301e2604e223f7f9fb0ae6f996e924b42248
JoseMedina <JoseMedina@DESKTOP-J6GQEMM>
2023-06-01 21:18:57 +0200

    conectando con react

--- ProyectoExtreme/Program.cs
+++ ProyectoExtreme/Program.cs
@@ -6,12 +6,30 @@ using ProyectoExtreme_Negocio.Interfaces;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//VIDEO PARA ACTIVAR LAS CORS
+//https://www.youtube.com/watch?v=Y_qD79PLdeM
+//MIRAR TAMBIEN EN appsettings.json
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+//AÑADIDO
+var proveedor = builder.Services.BuildServiceProvider();
+var configuracion = proveedor.GetRequiredService<IConfiguration>();
+
+builder.Services.AddCors(opciones =>
+{
+    var frontendURL = configuracion.GetValue<string>("frontend_url");
+
+    opciones.AddDefaultPolicy(builder =>
+    {
+        builder.WithOrigins(frontendURL).AllowAnyMethod().AllowAnyHeader();
+    });
+});
+//--------------
 
 // CONFIGURAR EL ACCESO A DATOS
 var conn = builder.Configuration.GetConnectionString("AppConnection"); // crea una variable con la cadena de conexion
@@ -30,6 +48,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpMethodOverride();
+
+//AÑADIDO
+app.UseCors();
 
 app.UseAuthorization();
 
